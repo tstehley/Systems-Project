7c561ed376ab89f2a20567d3f20d03f4a9a5fe85
Talya Stehley <talya925@gmail.com>
2020-01-06 00:31:05 -0500

    Now with comments!

--- project.c
+++ project.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-#define MSTR 64
+#define MSTR 1024
 int isCompressed(char instr[]){
 	int len= strlen(instr);
 	if(instr[(len-1)] == 'z' && instr[(len-2)] == 'g'){
@@ -10,6 +10,7 @@ int isCompressed(char instr[]){
 	}
 }
 int main (int argc, char *argv[]){
+	/*Insures the necessary arguments are there*/
 	if(argc < 2){
 		printf("No input file provided.\n");
 	}
@@ -19,8 +20,11 @@ int main (int argc, char *argv[]){
 	}
 	FILE *cat;
 	FILE *grep;
+	/*Intermediate var used to connect*/
 	char lines[MSTR];
+	/*customizable command*/
 	char catstr[MSTR]="";
+	/*customizes commands to be run for arguments given*/
 	if(isCompressed(argv[1]) == 1){
 		strcat(catstr, "zcat ");
 	}else{
@@ -30,18 +34,22 @@ int main (int argc, char *argv[]){
 	strcat(catstr, argv[1]);
 	strcat(grepstr,argv[2]);
 	/*printf("Piping %s into %s \n",catstr,grepstr);*/
-	/*read from cat*/
+	/*read from cat - open stream to be reas*/
 	if(!(cat = popen(catstr,"r"))){
 		printf("Error catting file\n");
 		return 1;	
 	}
+	/*write to grep, open to stream*/
 	if(!(grep = popen(grepstr, "w"))){
 		printf("Grep error.\n");
 		return 1;
 	}
+	/*With those sockets open, this takes up to MSTR lines from
+	 * cat and pipes them into grep*/
 	while(fgets(lines,MSTR,cat)){
 		fputs(lines,grep);
 	}
+	/*Be kind, rewind*/
 	pclose(cat);
 	pclose(grep);
 	return 0;
